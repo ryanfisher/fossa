e919e36c0f5b5633312ad8546a8a5e369b076065
Ryan Fisher <ryancfisher@gmail.com>
2018-01-16 23:46:05 -0800

    Add Fossa.HttpClient

---
+++ lib/fossa/http_client.ex
@@ -0,0 +1,13 @@
+defmodule Fossa.HttpClient do
+  def request(url) when is_bitstring(url) do
+    IO.puts("Requesting: #{url}")
+    with %HTTPoison.Response{body: body, status_code: 200} <- HTTPoison.get! url do
+      if String.valid?(body), do: body, else: nil
+    else
+      _ -> nil
+    end
+  rescue
+    _ in HTTPoison.Error -> ""
+  end
+  def request(_), do: ""
+end
--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Fossa.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 0.11"}
     ]
   end
 end
