8c9b5351ec91ad73a9af0f8e0ebc259c0507ed95
Ryan Fisher <ryancfisher@gmail.com>
2018-02-15 18:41:50 -0800

    Add URI module to help with uri manipulation

---
+++ lib/fossa/utils/uri.ex
@@ -0,0 +1,40 @@
+defmodule Fossa.Utils.URI do
+  @web_page_ext ["", ".html", ".html", ".php"]
+  @doc """
+  Prepends the host when given a relative path, does nothing with absolute
+
+  ## Examples
+
+    iex> Fossa.Utils.URI.prepend_host("/resume.html", "https://ryanfisher.io")
+    "https://ryanfisher.io/resume.html"
+    iex> Fossa.Utils.URI.prepend_host("http://pantryio.com/recipes", "https://pantryio.com")
+    "http://pantryio.com/recipes"
+  """
+  def prepend_host(href, url) do
+    %URI{host: site_host} = URI.parse(url)
+    %URI{host: host, scheme: scheme} = URI.parse(href)
+    if (scheme == "http" || scheme == "https" || scheme == nil) && (host == site_host || host == nil) do
+      url |> URI.merge(href) |> URI.to_string
+    else
+      false
+    end
+  end
+
+  @doc """
+  Checks if the given url looks like a web page
+
+  ## Examples
+
+    iex> Fossa.Utils.URI.potential_web_page?("/resume.pdf")
+    false
+    iex> Fossa.Utils.URI.potential_web_page?("/resume.html")
+    true
+  """
+  def potential_web_page?(url) when is_bitstring(url) do
+    @web_page_ext
+    |> Enum.find(&(&1 == Path.extname(url)))
+    |> is_nil
+    |> Kernel.not
+  end
+  def potential_web_page?(_), do: false
+end
---
+++ test/fossa/utils/uri_test.exs
@@ -0,0 +1,5 @@
+defmodule Fossa.Utils.URITest do
+  use ExUnit.Case
+
+  doctest Fossa.Utils.URI
+end
